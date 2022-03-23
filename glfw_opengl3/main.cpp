c081c3c92169c9cd5428363eb00779232d99d740
Aidavdw EE <dozyjones@gmail.com>
2022-03-23 09:19:49 +0100

    Fixed small error where gcc would not pick up headers in main

--- glfw_opengl3/main.cpp
+++ glfw_opengl3/main.cpp
@@ -1,6 +1,7 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include <memory>
 #include <stdio.h>
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
